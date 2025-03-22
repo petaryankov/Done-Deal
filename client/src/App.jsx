b37d55077b332ff5aeead577b92d127395e88ab1
Petar Yankov <yankov.petar4@gmail.com>
2025-03-22 18:14:20 +0200

    rout to product item added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Catalog from './components/products/catalog/Catalog'
+import CatalogItem from './components/products/catalog-item/CatalogItem'
 import './App.css'
-import Catalog from './components/catalog/Catalog'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/catalog' element={<Catalog />} />
+        <Route path='/catalog/:productId' element={<h1>ProductItem</h1>} />
         <Route path='/register' element={<h1>Register</h1>} />
         <Route path='/create' element={<h1>Create</h1>} />
         <Route path='/account' element={<h1>Account</h1>} />
