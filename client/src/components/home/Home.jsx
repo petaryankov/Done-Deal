7727af64d7484f43062588644909854081281019
Petar Yankov <yankov.petar4@gmail.com>
2025-03-22 19:01:00 +0200

    href changet to to

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 export default function Home() {
     return (
         <>
@@ -23,15 +25,15 @@ export default function Home() {
                             You can buy or sell items in our website.
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
-                            <a
-                                href="#"
-                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            <Link
+                                to="/catalog"
+                                className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Get started
-                            </a>
-                            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                            </Link>
+                            <Link to="#" className="text-sm/6 font-semibold text-gray-900">
                                 Learn more <span aria-hidden="true">→</span>
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
