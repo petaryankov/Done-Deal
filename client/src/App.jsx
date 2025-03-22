72ca24cde2a72d43d6f414cbf236b70b1853cfab
Petar Yankov <yankov.petar4@gmail.com>
2025-03-22 12:44:40 +0200

    header page with logo and routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Route, Routes } from 'react-router'
+import Header from './components/header/Header'
 import Home from './components/home/Home'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/catalog' element={<h1>Catalog</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/create' element={<h1>Create</h1>} />
+        <Route path='/account' element={<h1>Account</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+      </Routes>
     </>
   )
 }
