dac196efb552dc30e1de2dc71a3f69ecb223428e
Petar Yankov <yankov.petar4@gmail.com>
2025-03-23 14:55:40 +0200

    login and register pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Offers from './components/products/offers/Offers'
 import OfferDetails from './components/products/offer-details/OfferDetails'
 import ErrorNotFound from './components/error-not-found/ErrorNotFond'
 import './App.css'
+import Login from './components/users/login/Login'
+import Register from './components/users/regiter/Register'
 
 function App() {
 
@@ -15,10 +17,10 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/offers' element={<Offers />} />
                 <Route path='/offers/:offerId' element={<OfferDetails />} />
-                <Route path='/register' element={<h1>Register</h1>} />
+                <Route path='/register' element={<Register />} />
                 <Route path='/create' element={<h1>Create</h1>} />
                 <Route path='/account' element={<h1>Account</h1>} />
-                <Route path='/login' element={<h1>Login</h1>} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/*' element={<ErrorNotFound />} />
             </Routes>
         </>
---
+++ client/src/components/users/regiter/Register.jsx
@@ -0,0 +1,105 @@
+import { Link } from "react-router";
+
+export default function Register() {
+    return (
+      <>
+        {/* 
+          This example requires updating your template:
+          ``` 
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */}
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-6">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-red-600">
+              Create your account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                  Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    autoComplete="new-password"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label htmlFor="confirmPassword" className="block text-sm/6 font-medium text-gray-900">
+                  Confirm Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    type="password"
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Create Account
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+              Already have an account?{' '}
+              <Link to="/login" className="ml-2 font-semibold text-red-600 hover:text-red-700">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
