4ac1890f6258722c836634a8df08625a9332dbf8
Petar Yankov <yankov.petar4@gmail.com>
2025-03-23 12:26:44 +0200

    loader created

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,9 @@
+
+export default function Loader() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="animate-spin rounded-full border-t-4 border-blue-500 border-solid w-16 h-16"></div>
+        </div>
+    );
+
+}
