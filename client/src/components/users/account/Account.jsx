e6e7ac45d0ad79ca2f41798235ced9ebb76795a4
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 18:47:48 +0300

    account modified

--- client/src/components/users/account/Account.jsx
+++ client/src/components/users/account/Account.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router';
+import { UserContext } from '../../../api/contexts/UserContext';
 
 export default function Account() {
-  const [user, setUser] = useState({
-    username: 'JohnDoe123',
-    email: 'john.doe@example.com',
-    firstName: 'John',
-    lastName: 'Doe',
-  });
+  const {username, email} = useContext(UserContext);
 
   return (
     <>
@@ -34,7 +30,7 @@ export default function Account() {
                     id="username"
                     name="username"
                     type="text"
-                    value={user.username}
+                    value={username}
                     readOnly
                     className="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 sm:text-sm"
                   />
@@ -53,119 +49,14 @@ export default function Account() {
                     id="email"
                     name="email"
                     type="email"
-                    value={user.email}
+                    value={email}
                     readOnly
                     className="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 sm:text-sm"
                   />
                 </div>
               </div>
-
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-gray-900"
-                >
-                  Full Name
-                </label>
-                <div className="mt-2 flex space-x-2">
-                  <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    value={user.firstName}
-                    readOnly
-                    className="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 sm:text-sm"
-                  />
-                  <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    value={user.lastName}
-                    readOnly
-                    className="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 sm:text-sm"
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* Change Password */}
-            <div className="space-y-4">
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="currentPassword"
-                  className="block text-sm font-medium text-gray-900"
-                >
-                  Current Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="currentPassword"
-                  name="currentPassword"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
-                />
-              </div>
-
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="newPassword"
-                  className="block text-sm font-medium text-gray-900"
-                >
-                  New Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="newPassword"
-                  name="newPassword"
-                  type="password"
-                  required
-                  autoComplete="new-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
-                />
-              </div>
-
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="confirmPassword"
-                  className="block text-sm font-medium text-gray-900"
-                >
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  required
-                  autoComplete="new-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save Changes
-              </button>
             </div>
           </form>
-
-          <div className="mt-6 text-center">
-            <Link
-              to="/settings"
-              className="font-semibold text-red-600 hover:text-red-700"
-            >
-              Account Settings
-            </Link>
-          </div>
         </div>
       </div>
     </>
