866bcb4d1b462ad03973a0ebb06b44642a0aeb11
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 14:14:29 +0300

    user context added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,24 +9,35 @@ import Register from './components/users/regiter/Register'
 import Account from './components/users/account/Account'
 import CreateOffer from './components/products/create-offer/createOffer'
 import EditOffer from './components/products/edit-offer/EditOffer'
+import { useState } from 'react'
+import { UserContext } from './api/contexts/UserContext'
 import './App.css'
 
 function App() {
 
+    const [authData, setAuthData] = useState({});
+
+    const userLoginHandler = (data) => {
+        setAuthData(data);
+
+    };
+
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/offers' element={<Offers />} />
-                <Route path='/offers/:offerId' element={<OfferDetails />} />
-                <Route path='/edit/:offerId' element={<EditOffer />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/create' element={<CreateOffer/>} />
-                <Route path='/account' element={<Account />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/*' element={<ErrorNotFound />} />
-            </Routes>
+            <UserContext.Provider value={{...authData, userLoginHandler}}>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/offers' element={<Offers />} />
+                    <Route path='/offers/:offerId' element={<OfferDetails />} />
+                    <Route path='/edit/:offerId' element={<EditOffer />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/create' element={<CreateOffer />} />
+                    <Route path='/account' element={<Account />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/*' element={<ErrorNotFound />} />
+                </Routes>
+            </UserContext.Provider>
         </>
     )
 }
