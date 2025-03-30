bf402e3654f472baddd6ac805194e7ae5d38ea36
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 14:13:34 +0300

    user register hoock added

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -5,10 +5,19 @@ const baseUrl = 'http://localhost:3030/users';
 export const useLogin = () => {
 
     const login = async (email, password) => {
-        return request.post(`${baseUrl}/login`, {email, password});
+        request.post(`${baseUrl}/login`, { email, password });
     }
 
     return {
         login,
+    }
+}
+
+export const userRegister = () => {
+    const register = (email, password) => 
+        request.post(`${baseUrl}/register`, { email, password });
+    
+    return {
+        register,
     }
 }
