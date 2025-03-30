e584d4a830b8b1ebc85114cdd42beb6e267ac7ff
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 12:18:18 +0300

    login hoock created

---
+++ client/src/api/authApi.js
@@ -0,0 +1,14 @@
+import request from "../utils/request";
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const useLogin = () => {
+
+    const login = async (email, password) => {
+        return request.post(`${baseUrl}/login`, {email, password});
+    }
+
+    return {
+        login,
+    }
+}
