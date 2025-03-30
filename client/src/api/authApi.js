707d34f3d36ae671890c990a85d43c44e1ba43e4
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 18:41:42 +0300

    logout hoock created

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,4 +1,6 @@
+import { useContext, useEffect } from "react";
 import request from "../utils/request";
+import { UserContext } from "./contexts/UserContext";
 
 const baseUrl = 'http://localhost:3030/users';
 
@@ -12,11 +14,38 @@ export const useLogin = () => {
     }
 }
 
-export const userRegister = () => {
-    const register = (email, password) => 
-        request.post(`${baseUrl}/register`, { email, password });
+export const useRegister = () => {
+    const register = (email, password, username) => 
+        request.post(`${baseUrl}/register`, { email, password, username });
     
     return {
         register,
     }
+}
+
+export const useLogout = () => {
+    const { accessToken, userLogoutHandler} = useContext(UserContext);
+
+    useEffect(() => {
+
+        if (!accessToken) {
+            return;
+        }
+
+        const options = {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        }
+
+        request.get(`${baseUrl}/logout`, null, options)
+        .then(userLogoutHandler);
+
+    }, [accessToken, userLogoutHandler]);
+
+    return {
+        isLoggedOut: !!accessToken,
+    }
+
+    
 }
