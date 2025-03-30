494eec607b904d2a7b2f4a9f02fe7e8091cde6f2
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 18:42:35 +0300

    requester updated

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -8,12 +8,19 @@ const request = async (method, url, data, options = {}) => {
             ...options,
             headers: {
                 'Content-Type': 'application/json',
+                ...options.headers
             },
             body: JSON.stringify(data),
         }
     }
 
     const response = await fetch(url, options);
+
+    const responseContentType = response.headers.get('Content-Type');
+    
+    if (!responseContentType) {
+        return;
+    }
 
     const result = await response.json();
 
