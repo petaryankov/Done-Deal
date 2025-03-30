5acb00388b05f4b3f25b81c8e473264e08ddfff0
Petar Yankov <yankov.petar4@gmail.com>
2025-03-30 14:32:58 +0300

    useLogin body changed

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,9 +4,8 @@ const baseUrl = 'http://localhost:3030/users';
 
 export const useLogin = () => {
 
-    const login = async (email, password) => {
+    const login = async (email, password) => 
         request.post(`${baseUrl}/login`, { email, password });
-    }
 
     return {
         login,
