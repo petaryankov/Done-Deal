38b86b51fae74df8e33236fc1b848859d89de3a6
Petar Yankov <yankov.petar4@gmail.com>
2025-04-01 22:29:07 +0300

    text changed

--- client/src/components/products/offers/Offers.jsx
+++ client/src/components/products/offers/Offers.jsx
@@ -29,7 +29,7 @@ export default function Offers() {
         <>
             <div className="bg-white">
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-3xl text-center font-bold tracking-tight text-red-700">Offers</h2>
+                    <h2 className="text-3xl text-center font-bold tracking-tight text-red-700">All Offers</h2>
 
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {offers.map((offer) => (
