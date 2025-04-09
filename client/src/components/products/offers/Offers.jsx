fd7b6849315b32d4a924679e744ca9fd45cb6d80
Petar Yankov <yankov.petar4@gmail.com>
2025-04-09 22:51:02 +0300

    error handling in offers added

--- client/src/components/products/offers/Offers.jsx
+++ client/src/components/products/offers/Offers.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import Offer from "../offer/Offer";
 import Loader from "../../loader/Loader";
 import offerService from "../../../services/offerService";
+import ErrorMessage from "../../error-message/ErrorMessage";
 
 export default function Offers() {
     const [offers, setOffers] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
         setLoading(true);
@@ -13,6 +15,7 @@ export default function Offers() {
         offerService.getAll()
             .then(setOffers)
             .catch((error) => {
+                setErrorMessage(error.message);
                 console.error('Error fetching offer:', error);
             })
             .finally(() => {
@@ -29,13 +32,16 @@ export default function Offers() {
         <>
             <div className="bg-white">
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-3xl text-center font-bold tracking-tight text-red-700">All Offers</h2>
-
-                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        {offers.map((offer) => (
-                            <Offer key={offer._id} offer={offer} />
-                        ))}
-                    </div>
+                    <h2 className="mb-10 text-3xl text-center font-bold tracking-tight text-red-700">All Offers</h2>
+                    {offers.length > 0
+                        ?
+                        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                            {offers.map((offer) => (
+                                <Offer key={offer._id} offer={offer} />
+                            ))}
+                        </div>
+                        : <ErrorMessage error={errorMessage} />
+                    }
                 </div>
             </div>
         </>
